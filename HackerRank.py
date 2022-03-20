a35076bd7090b56784ce255fcf162cdc849797f8
SnorzMorz <68282318+SnorzMorz@users.noreply.github.com>
2022-03-20 16:31:21 +0000

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -736,3 +736,16 @@ def compress_string():
 def triangle_quest():
     for i in range(1, int(input()) + 1):  # More than 2 lines will result in 0 score. Do not leave a blank line also
         print(int(((10 ** i - 1) / (9)) ** 2))
+
+
+def set_intersection():
+    n = input()
+
+    english = set(input().split())
+
+    m = input()
+
+    french = set(input().split())
+
+    print(len(english.difference(french)))
+
