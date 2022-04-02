aacf9e87f00181a305c8db08f759b3a8983b4844
SnorzMorz <68282318+SnorzMorz@users.noreply.github.com>
2022-04-02 19:49:40 +0100

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -1093,3 +1093,37 @@ def isBalanced(s):
         return "YES"
     else:
         return "NO"
+
+
+def mergeLists(head1, head2):
+    if (head1.data > head2.data):
+        head = SinglyLinkedListNode(head2.data)
+        head2 = head2.next
+    else:
+        head = SinglyLinkedListNode(head1.data)
+        head1 = head1.next
+
+    current = head
+    while head1 and head2:
+        if (head1.data > head2.data):
+            node = SinglyLinkedListNode(head2.data)
+            current.next = node
+            current = current.next
+            head2 = head2.next
+        else:
+            node = SinglyLinkedListNode(head1.data)
+            current.next = node
+            current = current.next
+            head1 = head1.next
+
+    while head1:
+        current.next = head1
+        current = current.next
+        head1 = head1.next
+
+    while head2:
+        current.next = head2
+        current = current.next
+        head2 = head2.next
+
+    return head
